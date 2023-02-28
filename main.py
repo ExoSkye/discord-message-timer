a4e575323bd3ace8f5a71a8d4e80a8032e7b7674
ProtoKai <protobyte@duck.com>
2023-02-28 08:07:01 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import datetime
+
+import discord
+from discord.ext import tasks
+import sys
+
+CHANNEL_ID = int(sys.argv[1])
+MINUTES = int(sys.argv[2])
+
+client = discord.Client(intents=discord.Intents.messages)
+
+
+@tasks.loop(minutes=MINUTES)
+async def purge():
+    await client.wait_until_ready()
+    channel = client.get_channel(CHANNEL_ID)
+    async for message in channel.history():
+        if message.created_at < datetime.datetime.now() - datetime.timedelta(hours=24):
+            await message.delete()
+
+with open(".token") as f:
+    client.run(f.read().strip())
