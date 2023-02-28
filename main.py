f28190d0f4fb3277dc34f6895b6181f9c755567f
ProtoKai <protobyte@duck.com>
2023-02-28 08:09:17 +0000

    Fix logic error

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ MINUTES = int(sys.argv[2])
 client = discord.Client(intents=discord.Intents.messages)
 
 
-@tasks.loop(minutes=MINUTES)
+@tasks.loop(minutes=5)
 async def purge():
     await client.wait_until_ready()
     channel = client.get_channel(CHANNEL_ID)
     async for message in channel.history():
-        if message.created_at < datetime.datetime.now() - datetime.timedelta(hours=24):
+        if message.created_at < datetime.datetime.now() - datetime.timedelta(minutes=MINUTES):
             await message.delete()
 
 with open(".token") as f:
